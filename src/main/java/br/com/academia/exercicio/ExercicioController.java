6e5a0efe121a3d41f0859d801fac26eae3e6b028
Rosane Neckel <rosane@neckel.tech>
2026-08-23 20:24:41 -0300

    feat: Adicionado endpoint "lista-exercicios"

--- src/main/java/br/com/academia/exercicio/ExercicioController.java
+++ src/main/java/br/com/academia/exercicio/ExercicioController.java
@@ -3,6 +3,9 @@ package br.com.academia.exercicio;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class ExercicioController {
     @GetMapping("/boas-vindas")
@@ -15,6 +18,16 @@ public class ExercicioController {
         return new ExercicioDTO(1, "Sulpino", 3, 12);
     }
 
+    @GetMapping("/lista-exercicios")
+    public List<ExercicioDTO> getListaExercicios(){
+        List<ExercicioDTO> list = new ArrayList<>();
+        list.add(new ExercicioDTO(1, "Sulpino Reto com Barra", 4, 10));
+        list.add(new ExercicioDTO(2, "Agachamento Livre", 4, 12));
+        list.add(new ExercicioDTO(3, "Puxada Frontal na Polia", 3, 12));
+        list.add(new ExercicioDTO(4, "Desenvolvimento com Alteres", 3, 10));
+
+        return list;
+    }
 
 
 }
